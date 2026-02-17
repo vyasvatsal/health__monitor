5cc2d1e50f00bcfe46887c86bfb1e72fb68d6d18
unknown <vyasvatsal3@gmail.com>
2026-02-17 17:23:00 +0530

    vercel changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: [
+            '*',
+        ]);
         $middleware->alias([
             'subscribed' => \App\Http\Middleware\CheckSubscription::class,
         ]);
